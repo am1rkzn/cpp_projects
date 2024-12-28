9f3b8ff86e359bb072287dfaffdac53ea6600c4b
am1rkzn <valeevamir537@gmail.com>
2024-12-28 15:25:03 +0300

    Create greeting.cpp

---
+++ greeting.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Enter your name: ";
+    char name[40];
+    cin >> name;
+    cout << "Hello, " << name << endl;
+    return 0;
+}
