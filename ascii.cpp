c50e95a59d3d0fce8ba1b2d126ccaea362d7cedc
am1rkzn <valeevamir537@gmail.com>
2025-01-13 19:38:54 +0300

    Create ascii.cpp

---
+++ ascii.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Enter a character: ";
+    char ch1;
+    cin >> ch1;
+    cout<< "The ASCII value of character is: " << (int)ch1;
+}
