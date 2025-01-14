ed1ed1d264d3ccf42d1494ddf452dd463d4d7aab
am1rkzn <valeevamir537@gmail.com>
2025-01-14 21:47:29 +0300

    Create datatype_size.cpp

---
+++ datatype_size.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a = 10;
+    float b = 2.5;
+    double c = 3.5;
+    char d = 'a';
+    cout << "size of int: " << sizeof(a) << endl;
+    cout << "size of float: "<< sizeof(b) << endl;
+    cout << "size of double: "<< sizeof(c) << endl;
+    cout << "size of char: " << sizeof(d) << endl;
+}
